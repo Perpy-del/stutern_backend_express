426cb3a945fa714971012795ec163aed4ec15ec0
Perpy-del <perpetualmeninwa@gmail.com>
2023-04-26 10:11:01 +0100

    Node API

---
+++ NodeJS-API/README.md
@@ -0,0 +1,16 @@
+# Express In depth
+
+In this module, we were taught how to use HTTP verbs to perform CRUD operations. And how API calls work.
+
+### Tasks
+
+- Create a nodejs server, using express and axios
+
+- Setup an API that when called will return a new random dad’s joke from
+  [RapidDadJokes](https://rapidapi.com/KegenGuyll/api/dad-jokes) - You can create an account for yourself or can use the api-key below to gain access to the dad jokes api, mind you if you decide to use your own account, remember to subscribe to dad jokes before your api key will work. - You can do this in the pricing section of the dashboard.
+
+### _Apikey :“ac36004e64msh2f03d5e28e75ab5p1a32bfjsn9dc87404b637”_
+
+### Solution
+
+The solution to this task can be found in [File](./server.js)
---
+++ NodeJS-API/server.js
@@ -0,0 +1,36 @@
+const axios = require("axios");
+
+// Create a get request to get a random joke using the API URL
+const fetchDadJokes = {
+  // Get method
+  method: "GET",
+  // API URL
+  url: "https://dad-jokes.p.rapidapi.com/random/joke",
+  // Headers containing the content type, API key and API Host
+  headers: {
+    "content-type": "application/json",
+    "X-RapidAPI-Key": "85e4497a49msh78d19b288d2c227p1592c6jsn862d0f708ac1",
+    "X-RapidAPI-Host": "dad-jokes.p.rapidapi.com",
+  },
+};
+
+//   Using aync-await keywords and the try-catch block to get the response from the server
+async function getDadJokes() {
+  try {
+    const response = await axios.request(fetchDadJokes);
+    // console.log(response.data);
+    console.log(
+      `Title: ${response.data.body[0].type} \n Joke: ${response.data.body[0].setup}, ${response.data.body[0].punchline}`
+    );
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+getDadJokes();
+
+// Everytime you run node(node server.js), it comes up with a joke in the format below.
+
+/* Title: period
+ Joke: What did the Scottish lass say when she heard there would be universal free period products? Everyone! All together now!, “It’s about bloody time!” 
+ */
